e455089d4fe662115021efd060fccd3f6cc3409e
js725470 <josh-0505@hotmail.com>
2017-05-04 18:05:41 -0300

    complete

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private EditText replace;
+    private TextView display;
+
+    public void buttonOnClick(View v){
+        replace = (EditText) findViewById(R.id.textView2);
+        display = (TextView) findViewById(R.id.helloText);
+        display.setText(replace.getText());
+    }
+
 }
 //sdfsdf
